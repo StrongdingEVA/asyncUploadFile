5b291355f8ccfdcd9c2739a26baa039256913e7a
StrongdingEVA <18016550990@163.com>
2018-07-03 16:43:50 +0800

    'modify'

--- js/asyncUploadFile.js
+++ js/asyncUploadFile.js
@@ -2,16 +2,19 @@ jQuery.fn.extend({
     asyncUpload:function(cfg){
         var cfg = cfg;
         this.cfg = {};
-        this.cfg.filePanel = cfg.filePanel; //
-        this.cfg.btnFile = cfg.btnFile || ""; //文件域选择器
+        this.cfg.filePanel = cfg.filePanel; //文件域选择器
         this.cfg.upField = cfg.upField || "file"; //文件域名称
-        this.cfg.autoUpload = cfg.autoUpload || true; //默认开启自动上传
+        this.cfg.autoUpload = cfg.autoUpload === false ? cfg.autoUpload : true; //默认开启自动上传
         this.cfg.alowType = cfg.alowType || ["jpg","jpeg","gif","png"]; //默认允许文件后缀
         this.cfg.maxSize = cfg.maxSize || 1024 * 1024 * 2; //默认最大上传尺寸2M
         this.cfg.imgPanel = cfg.imgPanel || ""; //盛放image的容器
         this.cfg.cliText = cfg.cliText || "点击上传"; //未开启自动上传时 生成的按钮的文字
+        this.cfg.cliBtn = cfg.cliBtn || ''; //手动点击按钮
         this.cfg.maxLen = cfg.maxLen || 1; //默认允许最多上传文件个数
         this.cfg.upUrl = cfg.upUrl || ''; //文件上传路径
+        this.cfg.modelLimt = 1024 * 1024 * 8; //文件大于这个尺寸使用分片上传
+        this.cfg.sliceSize = 1024 * 1024; //没片大小
+        this.cfg.model = cfg.model || 1; //文件上传方式 1普通上传 2 分片上传
         this.cfg.callBack = cfg.callBack || {};
         this.cfg.files = [];
 
@@ -22,97 +25,73 @@ jQuery.fn.extend({
         // 'PD9waHA=' === '<?php'
         // 'PHNjcmlwdD4=' ==== '<script>'
 
-        this.check(this);
+        this._init(this.cfg);
     },
-    check: function(cfg) {
-        if(!cfg.btnFile){
+    _init: function(cfg) {
+        if(!cfg.filePanel || $(cfg.filePanel).length != 1){
             this.setError(this.getError(1));return;
         }else if(!cfg.upUrl){
             this.setError(this.getError(2));return;
-        }else if(!cfg.filePanel){
-            this.setError(this.getError(2));return;
+        }else if(!cfg.autoUpload){
+            if(!cfg.cliBtn || $(cfg.cliBtn).length != 1){
+                this.setError(this.getError(8));return;
+            }
         }
-        console.log(this.cfg);
+
+        //为容器绑定点击事件
         var that = this;
         $(cfg.filePanel).bind('click',function(){
-            var inp = $('<input type="file" style="display:none">')
+            var inp = $('<input type="file" style="display:none">');
             $(this).after(inp);
-            inp.bind('change',that.bindChange);
-            // inp.bind('change',function(){
-            //     var files = !!this.files ? this.files : [];
-            //     if (!files.length || !window.FileReader){
-            //         that.setError(that.getError(5));return;
-            //     }
-            //     files = files[0];
-            //     that.cfg.fileExt = files.name.substr(files.name.indexOf('.') + 1)
-            //     that.cfg.files.push(files);
-            //     that.cfg.fileSize = files.size;
-
-            //     if(!that.checkAlow()){
-            //         that.setError(that.getError(3));return;
-            //     }
-            //     var ckSize = that.checkSize();
-
-            //     if((typeof ckSize) != "boolean"){
-            //         that.setError(that.getError(ckSize));return;
-            //     }
-
-            //     if (/^image/.test(files.type)){
-            //         var reader = new FileReader();
-            //         reader.readAsDataURL(files);
-            //         reader.onloadend = function(){
-            //             if(!that.checkHex(this.result)){
-            //                 that.setError(that.getError(7));return;
-            //             }
-            //             that.setImg(this.result);
-            //         }
-            //         if(that.cfg.autoUpload){
-            //             that.doUpload();
-            //         }
-            //     }else{
-            //         that.setError(that.getError(3));return;
-            //     }
-            // })
-
+            that.bindChange(inp);
             inp.click();
         })
+
+        //为上传按钮绑定点击事件
+        if(cfg.autoUpload === false){
+            $(cfg.cliBtn).bind('click',function(){
+                that.doUpload();
+            })
+        }
     },
 
-    bindChange:function(e){
-        var that = this;console.log(this);
-        var files = !!e.currentTarget.files ? e.currentTarget.files : [];
-        if (!files.length || !window.FileReader){
-            that.setError(that.getError(5));return;
-        }
-        files = files[0];
-        that.cfg.fileExt = files.name.substr(files.name.indexOf('.') + 1)
-        that.cfg.files.push(files);
-        that.cfg.fileSize = files.size;
+    bindChange:function(ele){
+        var that = this;
+        ele.bind('change',function(){
+            var files = !!this.files ? this.files : [];
+            if (!files.length || !window.FileReader){
+                that.setError(that.getError(5));return;
+            }
+            files = files[0];
+            that.cfg.fileExt = files.name.substr(files.name.indexOf('.') + 1)
+            that.cfg.files.push(files);
+            that.cfg.fileSize = files.size;
 
-        if(!that.checkAlow()){
-            that.setError(that.getError(3));return;
-        }
-        var ckSize = that.checkSize();
+            if(!that.checkAlow()){
+                that.setError(that.getError(3));return;
+            }
+            var ckSize = that.checkSize();
 
-        if((typeof ckSize) != "boolean"){
-            that.setError(that.getError(ckSize));return;
-        }
+            if((typeof ckSize) != "boolean"){
+                that.setError(that.getError(ckSize));return;
+            }
 
-        if (/^image/.test(files.type)){
-            var reader = new FileReader();
-            reader.readAsDataURL(files);
-            reader.onloadend = function(){
-                if(!that.checkHex(this.result)){
-                    that.setError(that.getError(7));return;
+            if (/^image/.test(files.type)){
+                var reader = new FileReader();
+                reader.readAsDataURL(files);
+                reader.onloadend = function(){
+                    if(!that.checkHex(this.result)){
+                        that.setError(that.getError(7));return;
+                    }
+                    that.setImg(this.result);
                 }
-                that.setImg(this.result);
+                if(that.cfg.autoUpload){
+                    that.doUpload();
+                }
+            }else{
+                that.setError(that.getError(3));return;
             }
-            if(that.cfg.autoUpload){
-                that.doUpload();
-            }
-        }else{
-            that.setError(that.getError(3));return;
-        }
+        });
     },
 
     //检查文件是否允许上传
@@ -170,15 +149,19 @@ jQuery.fn.extend({
     },
     setPanel:function(){
         if(!this.cfg.imgPanel){
-            $(this).after('<div id="panel"></div>');
+            $(this).after('<div id="asyn_panel"></div>');
         }
-        this.cfg.imgPanel = '#panel';
+        this.cfg.imgPanel = '#asyn_panel';
     },
 
     //上传文件
     doUpload:function(){
         var that = this;
-        that.cfg.files.map(function(a){
+         var files = that.cfg.files;
+         if(!files.length){
+             this.setError(this.getError(9));return;
+         }
+        files.map(function(a){
             var formData = new FormData();
             formData.append(that.cfg.upField, a);
             $.ajax({
@@ -210,7 +193,7 @@ jQuery.fn.extend({
     getError:function(k){
         switch (k){
             case 1:
-                return "未设置文件域";
+                return "未设置或找不到" + this.cfg.filePanel + '元素';
             break;
             case 2:
                 return "未设置文件文件上传路径";
@@ -229,6 +212,12 @@ jQuery.fn.extend({
                 break;
             case 7:
                 return "文件含有可执行文件";
+                break;
+            case 8:
+                return "请设置手动上传按钮";
+                break;
+            case 9:
+                return "没有可以上传的文件";
                 break;
         }
     }
